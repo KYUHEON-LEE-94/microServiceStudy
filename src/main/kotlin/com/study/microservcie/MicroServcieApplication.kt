137ab0196880fe56ed878585bec94d4d605f1c7d
khlee <panpan68@naver.com>
2024-07-01 18:32:08 +0900

    source commit

---
+++ src/main/kotlin/com/study/microservcie/MicroServcieApplication.kt
@@ -0,0 +1,11 @@
+package com.study.microservcie
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MicroServcieApplication
+
+fun main(args: Array<String>) {
+    runApplication<MicroServcieApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/microservcie/MicroServcieApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.microservcie
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicroServcieApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
