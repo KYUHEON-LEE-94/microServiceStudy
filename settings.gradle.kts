c57e8f1070373b13710d9e5c559ee6db1a4b7766
Maizurzu <panpan68@naver.com>
2024-07-13 17:44:52 +0900

    api-gateway 어플리케이션 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,5 +6,5 @@ pluginManagement {
 
 }
 rootProject.name = "microService"
-include(":order-service", ":inventory-service", ":product", ":discovery-server")
+include(":order-service", ":inventory-service", ":product", ":discovery-server", ":api-gateway")
 
