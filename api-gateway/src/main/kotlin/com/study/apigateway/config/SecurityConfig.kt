3262a0f2220cd82e1c717695bdb9bebecf09dd65
Maizurzu <panpan68@naver.com>
2024-07-14 14:55:45 +0900

    api-gateway에 oauth2 설정 추가

---
+++ api-gateway/src/main/kotlin/com/study/apigateway/config/SecurityConfig.kt
@@ -0,0 +1,47 @@
+package com.study.apigateway.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+/**
+ *packageName    : com.study.apigateway.config
+ * fileName       : SecurityConfig
+ * author         : LEE KYUHEON
+ * date           : 2024-07-14
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-07-14        LEE KYUHEON       최초 생성
+ */
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+
+    @Bean
+    fun springSecurityFilterChain(serverHttpSecurity: ServerHttpSecurity): SecurityWebFilterChain {
+        return serverHttpSecurity
+            .csrf { it.disable() }
+            .authorizeExchange { exchanges ->
+                exchanges
+                    .pathMatchers("/eureka/**").permitAll()
+                    .anyExchange().authenticated()
+            }
+            .oauth2ResourceServer { oauth2 ->
+                oauth2.jwt { jwtConfigurer ->
+                    jwtConfigurer.jwtDecoder(jwtDecoder())
+                }
+            }
+            .build()
+    }
+
+    @Bean
+    fun jwtDecoder(): ReactiveJwtDecoder {
+        return NimbusReactiveJwtDecoder.withJwkSetUri("http://localhost:8181/realms/spring-boot-microservices-realm").build()
+    }
+}
