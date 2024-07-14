8cf4f8ca8026c88ee17736f4d1d1211d5959406f
Maizurzu <panpan68@naver.com>
2024-07-14 21:25:21 +0900

    api-gateway에 oauth2 설정 PostMan 확인

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -21,15 +21,11 @@ repositories {
 extra["springCloudVersion"] = "2023.0.2"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.1.2")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 dependencyManagement {
--- api-gateway/src/main/kotlin/com/study/apigateway/config/SecurityConfig.kt
+++ api-gateway/src/main/kotlin/com/study/apigateway/config/SecurityConfig.kt
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
 import org.springframework.security.web.server.SecurityWebFilterChain
 
 /**
@@ -32,16 +30,8 @@ class SecurityConfig {
                     .pathMatchers("/eureka/**").permitAll()
                     .anyExchange().authenticated()
             }
-            .oauth2ResourceServer { oauth2 ->
-                oauth2.jwt { jwtConfigurer ->
-                    jwtConfigurer.jwtDecoder(jwtDecoder())
-                }
+            .oauth2ResourceServer { oauth2 -> oauth2.jwt()
             }
             .build()
-    }
-
-    @Bean
-    fun jwtDecoder(): ReactiveJwtDecoder {
-        return NimbusReactiveJwtDecoder.withJwkSetUri("http://localhost:8181/realms/spring-boot-microservices-realm").build()
     }
 }
