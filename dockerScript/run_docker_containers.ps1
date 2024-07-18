8e075e1b767bbf9e34e1865b9965ebce246c7b9c
Maizurzu <panpan68@naver.com>
2024-07-18 22:01:17 +0900

    container 가 있는 경우는 그냥 실행 없는 경우 생성 구문 추가

--- dockerScript/run_docker_containers.ps1
+++ dockerScript/run_docker_containers.ps1
@@ -1,26 +1,38 @@
-# PowerShell 스크립트를 사용하여 Docker 컨테이너 실행
+# PowerShell 스크립트를 사용하여 Docker 컨테이너 실행 또는 시작
 
-# Zipkin 컨테이너 실행
-Write-Output "Starting Zipkin container..."
-docker run -d --name zipkinCon -p 9411:9411 openzipkin/zipkin
+# Function to start or create a Docker container
+function Start-OrCreateContainer {
+    param (
+        [string]$containerName,
+        [string]$runCommand
+    )
 
-# keycloak
-Write-Output "Starting Keycloak container..."
-docker run -d --name keycloakCon -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:25.0.1 start-dev
+    $container = docker ps -a -q -f name=$containerName
+    if ($container) {
+        Write-Output "Starting existing container: $containerName..."
+        docker start $containerName
+    } else {
+        Write-Output "Creating and starting new container: $containerName..."
+        Invoke-Expression $runCommand
+    }
+}
 
-# MongoDB 컨테이너 실행
-Write-Output "Starting MongoDB container..."
-docker run -d --name mongo -p 27017:27017 -e MONGO_INITDB_DATABASE=product-service mongo
+# Zipkin 컨테이너 실행 또는 생성
+Start-OrCreateContainer -containerName "zipkinCon" -runCommand "docker run -d --name zipkinCon -p 9411:9411 openzipkin/zipkin"
 
-# MySQL 컨테이너 실행 (oder-service)
-Write-Output "Starting MySQL container for order-service..."
-docker run -d --name mysql-container -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=oder-service -p 3306:3306 -d mysql:latest
+# Keycloak 컨테이너 실행 또는 생성
+Start-OrCreateContainer -containerName "keycloakCon" -runCommand "docker run -d --name keycloakCon -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:25.0.1 start-dev"
 
-# MySQL 컨테이너 실행 (inventory-service)
-Write-Output "Starting MySQL container2 for inventory-service..."
-docker run -d --name mysql-container2 -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=inventory-service -p 3307:3306 -d mysql:latest
+# MongoDB 컨테이너 실행 또는 생성
+Start-OrCreateContainer -containerName "mongo" -runCommand "docker run -d --name mongo -p 27017:27017 -e MONGO_INITDB_DATABASE=product-service mongo"
 
-Write-Output "All containers started successfully."
+# MySQL 컨테이너 실행 또는 생성 (order-service)
+Start-OrCreateContainer -containerName "mysql-container" -runCommand "docker run -d --name mysql-container -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=oder-service -p 3306:3306 mysql:latest"
+
+# MySQL 컨테이너 실행 또는 생성 (inventory-service)
+Start-OrCreateContainer -containerName "mysql-container2" -runCommand "docker run -d --name mysql-container2 -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=inventory-service -p 3307:3306 mysql:latest"
+
+Write-Output "All containers started or created successfully."
 
 # 실행 명령어
 # .\run_docker_containers.ps1
--- dockerScript/run_docker_containers.sh
+++ dockerScript/run_docker_containers.sh
@@ -1,23 +1,30 @@
-# Bash 스크립트를 사용하여 Docker 컨테이너 실행
+#!/bin/bash
 
-# Zipkin 컨테이너 실행
-echo "Starting Zipkin container..."
-docker run -d --name zipkinCon -p 9411:9411 openzipkin/zipkin
+# Function to start or create a Docker container
+start_or_create_container() {
+    local container_name=$1
+    local run_command=$2
 
-# keycloak
-echo "Starting Keycloak container..."
-docker run -d --name keycloakCon -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:25.0.1 start-dev
+    if [ "$(docker ps -a -q -f name=${container_name})" ]; then
+        echo "Starting existing container: ${container_name}..."
+        docker start ${container_name}
+    else
+        echo "Creating and starting new container: ${container_name}..."
+        eval ${run_command}
+    fi
+}
 
-# MongoDB 컨테이너 실행
-echo "Starting MongoDB container..."
-docker run -d --name mongo -p 27017:27017 -e MONGO_INITDB_DATABASE=product-service mongo
+# Zipkin 컨테이너 실행 또는 생성
+start_or_create_container "zipkinCon" "docker run -d --name zipkinCon -p 9411:9411 openzipkin/zipkin"
 
-# MySQL 컨테이너 실행 (oder-service)
-echo "Starting MySQL container for order-service..."
-docker run -d --name mysql-container -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=oder-service -p 3306:3306 -d mysql:latest
+# Keycloak 컨테이너 실행 또는 생성
+start_or_create_container "keycloakCon" "docker run -d --name keycloakCon -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:25.0.1 start-dev"
 
-# MySQL 컨테이너 실행 (inventory-service)
-echo "Starting MySQL container2 for inventory-service..."
-docker run -d --name mysql-container2 -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=inventory-service -p 3307:3306 -d mysql:latest
+# MongoDB 컨테이너 실행 또는 생성
+start_or_create_container "mongo" "docker run -d --name mongo -p 27017:27017 -e MONGO_INITDB_DATABASE=product-service mongo"
 
-echo "All containers started successfully."
+# MySQL 컨테이너 실행 또는 생성 (order-service)
+start_or_create_container "mysql-container" "docker run -d --name mysql-container -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=oder-service -p 3306:3306 mysql:latest"
+
+# MySQL 컨테이너 실행 또는 생성 (inventory-service)
+start_or_create_container "mysql-container2" "docker run -d --name mysql-container2 -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=inventory-service -p 3307:3306 mysql:latest"
